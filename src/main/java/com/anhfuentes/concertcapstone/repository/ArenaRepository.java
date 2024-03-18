5b8877dfff2629c70fd8720dcb6d5d99d834becd
anhf0309 <anhf0309@gmail.com>
2024-03-17 22:23:58 -0700

    Repository methods added

--- src/main/java/com/anhfuentes/concertcapstone/repository/ArenaRepository.java
+++ src/main/java/com/anhfuentes/concertcapstone/repository/ArenaRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ArenaRepository extends JpaRepository <Arena, Long> {
+    Arena findArenaByLocation (String location);
+
+    Arena findArenaByName(String arena_name);
 }
--- src/main/java/com/anhfuentes/concertcapstone/repository/ConcertRepository.java
+++ src/main/java/com/anhfuentes/concertcapstone/repository/ConcertRepository.java
@@ -1,14 +1,17 @@
 package com.anhfuentes.concertcapstone.repository;
 
 import com.anhfuentes.concertcapstone.model.Concert;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
 
 @Repository
 public interface ConcertRepository extends JpaRepository <Concert, Long> {
-    List<Concert> findByDateTimeAfter (LocalTime date_time);
+    List<Concert> findByDateTimeAfter (LocalDateTime date_time);
+    List<Concert> findByGenre(String genre, Sort sort);
 }
 
