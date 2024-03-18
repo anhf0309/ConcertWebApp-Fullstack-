84a6d6417b73ec40e3a4a117b7e9dc0d6b661212
anhf0309 <anhf0309@gmail.com>
2024-03-18 01:27:31 -0700

    User Service Layer updated

--- src/main/java/com/anhfuentes/concertcapstone/service/UserService.java
+++ src/main/java/com/anhfuentes/concertcapstone/service/UserService.java
@@ -1,8 +1,12 @@
 package com.anhfuentes.concertcapstone.service;
 
 import com.anhfuentes.concertcapstone.model.User;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public interface UserService {
     User createUser(User user);
-    User getUserByUsername(String username);
+    User getUserByUsername(String username) throws UsernameNotFoundException;
+    User updateUser(User user);
+    User findUserByEmail(String email) throws UsernameNotFoundException;
+    boolean deleteUser(Long id) throws UsernameNotFoundException;
 }
--- src/main/java/com/anhfuentes/concertcapstone/service/UserServiceImpl.java
+++ src/main/java/com/anhfuentes/concertcapstone/service/UserServiceImpl.java
@@ -1,4 +1,51 @@
 package com.anhfuentes.concertcapstone.service;
 
-public class UserServiceImpl {
+import com.anhfuentes.concertcapstone.model.User;
+import com.anhfuentes.concertcapstone.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Override
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User getUserByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+    }
+
+    @Override
+    public User updateUser(User user) {
+        Long userId = user.getId();
+        if(userId != null && userRepository.existsById(userId)) {
+            return userRepository.save(user);
+        } else {
+            throw new UsernameNotFoundException("User not found with id: " + userId);
+        }
+    }
+
+    @Override
+    public User findUserByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with this email: " + email));
+    }
+
+    @Override
+    public boolean deleteUser(Long id) {
+        if (userRepository.existsById(id)) {
+            userRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
 }
