101038c82b2abb64aeaa44f69f4c61ec2a7b620d
anhf0309 <anhf0309@gmail.com>
2024-03-19 21:52:35 -0700

    User Controller updated

--- src/main/java/com/anhfuentes/concertcapstone/controller/UserController.java
+++ src/main/java/com/anhfuentes/concertcapstone/controller/UserController.java
@@ -3,18 +3,31 @@ package com.anhfuentes.concertcapstone.controller;
 import com.anhfuentes.concertcapstone.model.User;
 import com.anhfuentes.concertcapstone.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/api/users")
+@Controller
+@RequestMapping("/user")
 public class UserController {
 
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
 
-    @PostMapping("/signup")
-    public User createUser(@RequestBody User user) {
-        return userService.createUser(user);
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
+
+    @GetMapping("/profile")
+    public String userProfile(Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+        User user = userService.getUserByUsername(username);
+        model.addAttribute("user", user);
+        return "userProfile";
+    }
+
 
 }
